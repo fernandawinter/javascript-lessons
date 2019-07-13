6145587b9498e6b72400760011975a7c62b3fe83
Fernanda Winter <fernanda.winter@hotmail.com>
2019-07-13 17:57:44 -0300

    Adicionando lição 1 ao reposirtório

---
+++ lesson-1/minhasFuncoes.js
@@ -0,0 +1,26 @@
+var meuNome = 'Fernanda Winter2';
+
+function imprimeMeuNome() {
+    console.log(meuNome);
+}
+
+function fazMeuAlerta() {
+    alert(23);
+}
+
+function fazTudo() {
+    imprimeMeuNome();
+    fazMeuAlerta();
+}
+
+if (meuNome == 'Fernanda Winter') {
+    console.log('SOU EU MESMO'); 
+} else {
+    console.log('NÃO SOU EU MESMO');
+}
+
+if (2 === '2') {
+    console.log('Os 1s são iguais');
+} else {
+    console.log('OS 1S SÃO DIFERENTES');
+}
