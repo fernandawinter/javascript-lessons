14a50d223f65a80ea59d623738f85b82ffb6a01b
Fernanda Winter <fernanda.winter@hotmail.com>
2021-09-26 22:57:31 -0300

    adding another exercise: currency converter

--- lesson-15-grade-calculator/calculadora.js
+++ lesson-15-grade-calculator/calculadora.js
@@ -3,7 +3,7 @@ var notaPrimeiroBimestre = 9
 var notaSegundoBimestre = 7
 var notaTerceiroBimestre = 4
 var notaQuartoBimestre = 2
-  
+
 var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4
 
 var notaFixada = notaFinal.toFixed(1)
