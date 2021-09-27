2fe333094adf05a5a158f4693c174daf1c3fd218
Fernanda Winter <fernanda.winter@hotmail.com>
2021-09-26 23:43:13 -0300

    adding one more test

--- lesson-11-fizz-buzz/fizz-buzz.spec.js
+++ lesson-11-fizz-buzz/fizz-buzz.spec.js
@@ -1,41 +1,46 @@
-describe('Testing fizz-buzz.js:', ()=> {  
-    it('quando o número for 1 retornar só o número 1', ()=> { 
-      var resultado = fizzBuzz(1);
-      expect(resultado).toBe('1'); 
-    });
+describe('Testing fizz-buzz.js:', () => {
+  it('quando o número for 1 retornar só o número 1', () => {
+    var resultado = fizzBuzz(1);
+    expect(resultado).toBe('1');
+  });
 
-    it('quando o número for 4 retornar só o número 4', ()=> { 
-      var resultado = fizzBuzz(4);
-      expect(resultado).toBe('4'); 
-    });
+  it('quando o número for 4 retornar só o número 4', () => {
+    var resultado = fizzBuzz(4);
+    expect(resultado).toBe('4');
+  });
 
-    it('quando o número for 3 retornar Fizz', ()=> { 
-      var resultado = fizzBuzz(3);
-      expect(resultado).toBe('Fizz'); 
-    });
+  it('quando o número for 3 retornar Fizz', () => {
+    var resultado = fizzBuzz(3);
+    expect(resultado).toBe('Fizz');
+  });
 
-    it('quando o número for 5 retornar Buzz', ()=> { 
-      var resultado = fizzBuzz(5);
-      expect(resultado).toBe('Buzz'); 
-    });
+  it('quando o número for 5 retornar Buzz', () => {
+    var resultado = fizzBuzz(5);
+    expect(resultado).toBe('Buzz');
+  });
 
-    it('quando o número for 15 retornar FizzBuzz', ()=> { 
-      var resultado = fizzBuzz(15);
-      expect(resultado).toBe('FizzBuzz'); 
-    });
+  it('quando o número for 15 retornar FizzBuzz', () => {
+    var resultado = fizzBuzz(15);
+    expect(resultado).toBe('FizzBuzz');
+  });
 
-    it('quando o número for 21 retornar Fizz', ()=> { 
-      var resultado = fizzBuzz(21);
-      expect(resultado).toBe('Fizz'); 
-    });
+  it('quando o número for 21 retornar Fizz', () => {
+    var resultado = fizzBuzz(21);
+    expect(resultado).toBe('Fizz');
+  });
 
-    it('quando o número for 20 retornar Buzz', ()=> { 
-      var resultado = fizzBuzz(20);
-      expect(resultado).toBe('Buzz'); 
-    });
+  it('quando o número for 20 retornar Buzz', () => {
+    var resultado = fizzBuzz(20);
+    expect(resultado).toBe('Buzz');
+  });
 
-    it('quando o número for 45 retornar FizzBuzz', ()=> { 
-      var resultado = fizzBuzz(45);
-      expect(resultado).toBe('FizzBuzz'); 
-    });
+  it('quando o número for 45 retornar FizzBuzz', () => {
+    var resultado = fizzBuzz(45);
+    expect(resultado).toBe('FizzBuzz');
+  });
+
+  it('quando o número for 25 retornar Buzz', () => {
+    var resultado = fizzBuzz(25);
+    expect(resultado).toBe('Buzz');
+  });
 });
